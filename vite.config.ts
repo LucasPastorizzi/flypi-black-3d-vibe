a2eaee7f4c7b283a05347dd7c1fb1b2cebeb137a
Lucas Pastorizzi <Lucasdasilvapastorizzi@gmail.com>
2025-11-05 19:49:58 -0300

    add new src

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/flypi-black-3d-vibe/", 
+  base: "/flypi-black-3d-vibe", 
 
   server: {
     host: "::",
